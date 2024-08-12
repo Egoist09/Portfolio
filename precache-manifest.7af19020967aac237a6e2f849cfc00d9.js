5e6f8f33956a84cca74502f4aefedfdb68c77539
Egoist09 <paulcardeno9@gmail.com>
2024-08-12 14:43:00 +0800

    Updates

---
+++ precache-manifest.7af19020967aac237a6e2f849cfc00d9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7152ebbc1910be35dc1a3a2e9679f2c7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8591ac12dd2ae6556727",
+    "url": "/portfolio/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "da1328f77e10842d2b07",
+    "url": "/portfolio/static/css/main.884d55aa.chunk.css"
+  },
+  {
+    "revision": "8591ac12dd2ae6556727",
+    "url": "/portfolio/static/js/2.ea88a379.chunk.js"
+  },
+  {
+    "revision": "02b6ba6d5c9cedd34156ea733fcb10ec",
+    "url": "/portfolio/static/js/2.ea88a379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7b211454fc2c771bad",
+    "url": "/portfolio/static/js/3.4ee5c9ad.chunk.js"
+  },
+  {
+    "revision": "da1328f77e10842d2b07",
+    "url": "/portfolio/static/js/main.15a06aa7.chunk.js"
+  },
+  {
+    "revision": "1a87eee339a285b152d2",
+    "url": "/portfolio/static/js/runtime-main.2136cae6.js"
+  },
+  {
+    "revision": "717e70c0aaa4ddf80a0c6d26977d8830",
+    "url": "/portfolio/static/media/About_page.717e70c0.jpg"
+  },
+  {
+    "revision": "57f9075487a4cef6fbfc114c67d78bc7",
+    "url": "/portfolio/static/media/Contact_page.57f90754.jpg"
+  },
+  {
+    "revision": "18e270e2dd81ecfcbeb2b89e3a282925",
+    "url": "/portfolio/static/media/Home_page.18e270e2.jpg"
+  },
+  {
+    "revision": "7ccdb1bd9fea9e9a70a37d1e483182d1",
+    "url": "/portfolio/static/media/about-button.7ccdb1bd.svg"
+  },
+  {
+    "revision": "e8264e5169385910b9b698f970872b73",
+    "url": "/portfolio/static/media/brand-logo.e8264e51.jpg"
+  },
+  {
+    "revision": "c595cbd5f677de0e8f78892cd66f624a",
+    "url": "/portfolio/static/media/facebook-icon.c595cbd5.svg"
+  },
+  {
+    "revision": "ac296231f3cfa24b12750bc0bfb3a9f2",
+    "url": "/portfolio/static/media/instagram-icon.ac296231.svg"
+  },
+  {
+    "revision": "07edabf9ee7b7386032d7ec66a11e7ec",
+    "url": "/portfolio/static/media/project1.07edabf9.png"
+  },
+  {
+    "revision": "a7f8d86d1efa1d1573b5ec937fec7248",
+    "url": "/portfolio/static/media/project2.a7f8d86d.png"
+  },
+  {
+    "revision": "20994235db871f392729663884f10a3f",
+    "url": "/portfolio/static/media/project3.20994235.png"
+  },
+  {
+    "revision": "b154c4e486eeb62c76f9275a580d3bf4",
+    "url": "/portfolio/static/media/project_background.b154c4e4.jpg"
+  },
+  {
+    "revision": "7ccdb1bd9fea9e9a70a37d1e483182d1",
+    "url": "/portfolio/static/media/projects-button.7ccdb1bd.svg"
+  },
+  {
+    "revision": "176ab4997cfd14eb2ef7ec1934554dc3",
+    "url": "/portfolio/static/media/tiktok-icon.176ab499.svg"
+  }
+]);
